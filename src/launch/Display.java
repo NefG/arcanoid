3a9dd519c157952c742d355e4dbe91ceb3839fb7
unknown <AoiR@wias-ws-030.wias.local>
2015-11-25 14:19:08 +0200

    initial commit

---
+++ src/launch/Display.java
@@ -0,0 +1,31 @@
+package launch;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+import arcanoid.Game;
+
+public class Display {
+	
+	public static final String TITLE = "Arcanoid";
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 640;
+	
+	public static void main(String[] args) {
+		
+		try {
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new Game(Display.TITLE));
+			appgc.setDisplayMode(Display.WIDTH, Display.HEIGHT, false);
+			appgc.setTargetFrameRate(60);
+			appgc.setShowFPS(true);
+			appgc.start();
+		} catch (SlickException ex) {
+			Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+	}
+}
